be161e7e27075d5e254aae654c990737331a0b49
Janghun Park <84391453+Energy-CEO@users.noreply.github.com>
2023-12-26 10:39:28 +0900

    test: [박장훈] account form 함수 호출 테스트 [PTANU-T1-9]

--- T1_UI/tests/custom_protocol/TestCustomProtocol.py
+++ T1_UI/tests/custom_protocol/TestCustomProtocol.py
@@ -1,5 +1,8 @@
 import unittest
 
+from account_form.repository.AccountFormRepository import AccountFormRepository
+from account_form.repository.AccountFormRepositoryImpl import AccountFormRepositoryImpl
+from custom_protocol.entity.CustomProtocol import CustomProtocol
 from custom_protocol.repository.CustomProtocolRepositoryImpl import CustomProtocolRepositoryImpl
 
 
@@ -15,11 +18,19 @@ class TestCustomProtocol(unittest.TestCase):
 
     def testCustomProtocolOperation(self):
         firstInstance = CustomProtocolRepositoryImpl.getInstance()
+        secondInstance = CustomProtocolRepositoryImpl.getInstance()
+        accountFormRepository = AccountFormRepositoryImpl.getInstance()
 
-        firstInstance.register(1, self.accountLoginFormGeneration)
-        firstInstance.execute(1)
-        firstInstance.register(2, self.accountRegisterFormGeneration)
-        firstInstance.execute(2)
+        # firstInstance.register(1, self.accountLoginFormGeneration)
+        # firstInstance.execute(1)
+        # firstInstance.register(2, self.accountRegisterFormGeneration)
+        # firstInstance.execute(2)
+
+        firstInstance.register(CustomProtocol.ACCOUNT_LOGIN.value, accountFormRepository.createAccountSigninForm)
+        firstInstance.execute(0)
+        secondInstance.register(CustomProtocol.ACCOUNT_REGISTER.value,
+                               accountFormRepository.createAccountRegistrationForm)
+        secondInstance.execute(1)
 
 
 if __name__ == '__main__':
