617a02995c86e7ec96e4212899156d5eba700c23
Janghun Park <84391453+Energy-CEO@users.noreply.github.com>
2023-12-30 18:35:10 +0900

    update: [박장훈] command table, routing state converter 내용 추가 [PTANU-T1-9]

--- T1_UI/console_ui/entity/ConsoleUiRoutingState.py
+++ T1_UI/console_ui/entity/ConsoleUiRoutingState.py
@@ -3,7 +3,6 @@ from enum import Enum
 
 class ConsoleUiRoutingState(Enum):
     INITIALIZED = 0
-    ACCOUNT_REGISTER = 1
-    LOGIN = 2
-    LOGOUT = 3
-    ACCOUNT_DELETE = 4
+    PRODUCT_LIST = 1
+    PRODUCT_DETAILS = 2
+    ORDER_LIST = 3
