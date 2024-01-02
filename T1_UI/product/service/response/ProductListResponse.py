643ddf5eb603cbac588d22c7623f6c9153e47a1b
tkdrms420 <149447077+tkdrms420@users.noreply.github.com>
2024-01-02 12:53:19 +0900

    feat: [윤상근] ProductService단 구성해서 응답을 반응한다 [PTANU-T1-27]

---
+++ T1_UI/product/service/response/ProductListResponse.py
@@ -0,0 +1,8 @@
+class ProductListResponse:
+    __productList = []
+
+    def __init__(self, productList):
+        self.__productList = productList
+
+    def getProductList(self):
+        return self.__productList
