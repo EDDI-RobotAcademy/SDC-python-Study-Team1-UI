f2b851209bf54146ce79cae1b46096d3708c5f5c
Janghun Park <84391453+Energy-CEO@users.noreply.github.com>
2024-01-03 12:48:25 +0900

    test: [박장훈] list, dict 형태 data 처리를 위한 test [PTANU-T1-X]

--- T1_UI/tests/response_generator/TestResponseGenerator.py
+++ T1_UI/tests/response_generator/TestResponseGenerator.py
@@ -24,6 +24,37 @@ class TestResponseGenerator(unittest.TestCase):
         print(data)
         # key 값을 맞추면 저희가 response 객체 내 private 변수에다 argument[0] 요런식으로
 
+    def testBoolTypeResponse(self):
+        # Booltype은 다 처리 가능 확인
+        # 1, 3, 4, 6, 8, 9, 10, 13
+        sampleResponse = {"protocolNumber": 4, "data": True}
+        receivedProtocolNumber = sampleResponse['protocolNumber']
+        receivedData = sampleResponse['data']
+        responseGeneratorService = ResponseGeneratorServiceImpl.getInstance()
+        responseGenerator = responseGeneratorService.findResponseGenerator(receivedProtocolNumber)
+        response = responseGenerator(receivedData)
+        print(response.getIsSuccess())
+
+    def testDictionaryResponse(self):
+        # 2 : account login 같은 경우는 아래와 같이 처리했습니다.
+        sessionId = {'__accountSessionId': 3}  # 현재 저장하고 있는 스타일이 왼쪽과 같은 dict 형태입니다.
+        # 따라서 아래와 같이 처리합니다.
+        print(int(sessionId.get('__accountSessionId')))
+
+        # 7, 12
+        sampleResponse = {"protocolNumber": 7, "data": {1, "Sibal", 20000, "Dog Sibal", "m,e"}}
+        receivedProtocolNumber = sampleResponse['protocolNumber']
+        receivedData = sampleResponse['data']
+        responseGeneratorService = ResponseGeneratorServiceImpl.getInstance()
+        responseGenerator = responseGeneratorService.findResponseGenerator(receivedProtocolNumber)
+        response = responseGenerator(receivedData)
+        print(response.getId())
+        # print(response[1])
+
+    def testListResponse(self):
+        # 5, 11
+        sampleResponse = {"protocolNumber": 7, "data": {1, "Sibal", 20000, "Dog Sibal", "m,e"}}
+
 
 if __name__ == '__main__':
     unittest.main()
